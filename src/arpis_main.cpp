d6d16bbd0a30a1c3f512f97cd8ca4ac4259b9803
marfanr <mohammadarfan45@gmail.com>
2022-11-23 16:29:41 +0700

    feat: implement arpis_network package

--- src/arpis_main.cpp
+++ src/arpis_main.cpp
@@ -6,42 +6,45 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include "rclcpp/rclcpp.hpp"
+#include "arpis_network/tcp/tcp.hpp"
+
+#include <math.h>
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+#define _USE_MATH_DEFINES
 
 using namespace std::chrono_literals;
 
 class ArpisClientNode : public rclcpp::Node {
   public:
   ArpisClientNode(): Node("client") {
-    // timer_ = this->create_wall_timer(400ms, std::bind(&ArpisClientNode::exec, this));
-    this->exec();
+    tcp = new arpis_network::tcp("127.0.0.1", 3030);
+    tcp->connect();
+    tcp_receiver = this->create_publisher<geometry_msgs::msg::Quaternion>("tcp_receiver", 5);
+    timer_ = this->create_wall_timer(400ms, std::bind(&ArpisClientNode::exec, this));
+    // this->exec();
   }
-  private:
-  void exec() {
-    sockaddr_in serv;
-    bzero(&serv, sizeof(serv));
-    char buffer[1024];
-    int sock;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      std::cout << "Faillied created socket" << std::endl;
-    } else 
-      std::cout << "success created socket" << std::endl;
-
-    // memset(&serv, '\0', sizeof(serv));
-    serv.sin_port = htons(3030);
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = inet_addr("127.0.0.1");        
-    if ( connect(sock, (sockaddr*)&serv, sizeof(serv)) != 0 ) {
-      std::cout << "failied sconnect server" << std::endl;
-    } else 
-      std::cout << "success connect server" << std::endl;
+  private:  
+  void rviz() {
 
   }
+  void exec() {    
+    char buffer[1024] = {0};
+    tcp->receive(buffer, 1024);
+    if (std::string(buffer).length() != 0)
+      std::cout << std::string(buffer) << std::endl;    
+  }  
+  rclcpp::Publisher<geometry_msgs::msg::Quaternion>::SharedPtr tcp_receiver;
+  rclcpp::Subscription<geometry_msgs::msg::Quaternion>::SharedPtr rcp_subscription;
   rclcpp::TimerBase::SharedPtr timer_;
+  arpis_network::tcp * tcp;
 };
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
+  // auto node = std::make_shared<rclcpp::Node>()
   rclcpp::spin(std::make_shared<ArpisClientNode>());
   rclcpp::shutdown();
   
